76b17db17d68cb2dc3b2c4d8f5ddbe192c7e7a36
waffe_x20 <argueta_eduardo@yahoo.de>
2024-01-01 22:45:54 -0600

    Add borrower to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Author, Book, BookInstance, Genre, Language
 
-
 admin.site.register(Genre)
 admin.site.register(Language)
 
@@ -66,10 +65,10 @@ class BookInstanceAdmin(admin.ModelAdmin):
      - grouping of fields into sections (fieldsets)
     """
 
-    list_display = ["book", "status", "due_back", "id"]
+    list_display = ["book", "status", "borrower", "due_back", "id"]
     list_filter = ["status", "due_back"]
 
     fieldsets = (
         (None, {"fields": ("book", "imprint", "id")}),
-        ("Availability", {"fields": ("status", "due_back")}),
+        ("Availability", {"fields": ("status", "due_back", "borrower")}),
     )
